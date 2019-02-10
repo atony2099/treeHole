615ac29287dd8e126e07dfa5c70d50867c76716e
Fenglin Tang <atony2099@gmail.com>
2019-02-11 00:03:48 +0800

    add alinode

--- config/plugin.js
+++ config/plugin.js
@@ -1,29 +1,34 @@
 /*
  * @Author: atony2099
  * @Date: 2018-12-01 23:28:04
- * @Last Modified by:   atony2099
- * @Last Modified time: 2018-12-01 23:28:04
+ * @Last Modified by: atony2099
+ * @Last Modified time: 2019-02-11 00:03:18
  */
-
 
 'use strict';
 
 exports.mongoose = {
   enable: true,
-  package: 'egg-mongoose',
+  package: 'egg-mongoose'
 };
 
 exports.validate = {
   enable: true,
-  package: 'egg-validate',
+  package: 'egg-validate'
 };
 
 exports.routerPlus = {
   enable: true,
-  package: 'egg-router-plus',
+  package: 'egg-router-plus'
 };
 
 exports.redis = {
   enable: true,
-  package: 'egg-redis',
+  package: 'egg-redis'
+};
+
+exports.alinode = {
+  enable: true,
+  package: 'egg-alinode',
+  env: ['prod']
 };
