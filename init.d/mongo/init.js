6a3b3fdd3f1b25c731582ed4f4495c00a0193f60
Fenglin Tang <atony2099@gmail.com>
2019-02-12 05:23:08 +0800

    update compose

--- init.d/mongo/init.js
+++ init.d/mongo/init.js
@@ -5,8 +5,8 @@
  * 2. create collection (Before MongoDB can save your new database, a collection name must also be specified at the time of creation.)
  */
 db.createUser({
-  user: 'egg_cnode',
-  pwd: 'egg_cnode',
+  user: 'tree_node',
+  pwd: 'tree_node',
   roles: [
     {
       role: 'readWrite',
@@ -15,6 +15,6 @@ db.createUser({
   ]
 });
 
-db.egg_cnode.insert({
-  egg_cnode: 'egg-cnode'
+db.tree_node.insert({
+  tree_node: 'tree-node'
 });
