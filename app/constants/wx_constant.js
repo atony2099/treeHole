7e3ea6fe1f115527b7ae1ca39c247c2d6b3f7592
Fenglin Tang <atony2099@gmail.com>
2019-02-12 13:16:39 +0800

    no message

--- app/constants/wx_constant.js
+++ app/constants/wx_constant.js
@@ -2,11 +2,11 @@
  * @Author: atony2099
  * @Date: 2018-12-11 14:52:09
  * @Last Modified by: atony2099
- * @Last Modified time: 2019-02-12 13:11:00
+ * @Last Modified time: 2019-02-12 13:16:32
  */
 
 'use strict';
 module.exports = {
-  WX_HEADER_FLAG: 'WX-HEADER-FLAG',
-  WX_HEADER_SKEY: 'WX-HEADER-SKEY'
+  WX_HEADER_FLAG: 'wx-header-flag',
+  WX_HEADER_SKEY: 'wx-header-skey'
 };
